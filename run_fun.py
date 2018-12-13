240e19a3c9acec766a784634731fc554bb71b510
Takuto <pysan3@gmail.com>
2018-12-14 06:30:25 +0900

    log

--- run_fun.py
+++ run_fun.py
@@ -1,7 +1,13 @@
 import responder
+from logging import getLogger, config
 import backend.app as backapp
 
 api = responder.API(debug=True, templates_dir='./dist', static_dir='./dist/static')
+
+config.fileConfig(
+    'logging.conf'
+)
+logger = getLogger('logExample')
 
 @api.route('/')
 async def index(req, resp):
